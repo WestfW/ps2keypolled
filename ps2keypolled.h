fea9ed61b76d04e2ba7ac847215e00be070357ae
westfw <westfw@5398c544-fe45-11dd-bc25-5f29055a2a2d>
2009-02-19 09:25:32 +0000

    initial import

---
+++ ps2keypolled.h
@@ -0,0 +1,31 @@
+/*
+ * ps2keypolled.h
+ * Copyright 2009 by Bill Westfield
+ *
+ * defines for polled interface to PS2-like keybaords.
+ */
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+    extern unsigned char ps2k_getcode(void);
+    extern int ps2k_getkey(void);
+    extern void ps2k_sendbyte(unsigned char);
+    extern unsigned char ps2k_translate(int);
+    extern void ps2k_init (unsigned char clock, unsigned char data);
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#include "WProgram.h"
+
+/*
+ * Value to add to "extended" keycodes.  Can be 0x80 to fit in a singlpe
+ * byte, which ALMOST never conflicts with anything.  Or it can be 100.
+ */
+#define PS2K_EXTEND 0x100
+
+#define PS2K_NOKEY 0xFF
+
+extern unsigned char ps2k_clk, ps2k_dat;
+
